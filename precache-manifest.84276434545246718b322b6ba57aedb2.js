c9384438331330c718ddb15736fc5cda46d3c9fb
riley-white <ronishawhite@gmail.com>
2021-04-07 18:28:18 -0700

    Updates

---
+++ precache-manifest.84276434545246718b322b6ba57aedb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70a26cf26e63e74c3956fd9b5f11b6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "084258de4ef1be1a6f9a",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "9707e1cac4d5d2cf6a1f",
+    "url": "/static/css/main.d449bbdc.chunk.css"
+  },
+  {
+    "revision": "084258de4ef1be1a6f9a",
+    "url": "/static/js/2.befda6fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.befda6fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9707e1cac4d5d2cf6a1f",
+    "url": "/static/js/main.de6791b6.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
